97c6d76e0513661d67c2106121328233d550e22f
Vasily Gerrans <vgerrans@gmail.com>
2024-05-09 16:28:24 +0100

    Revert "refactor: completely remove all remappings"
    This reverts commit 0761d88b58d064eaca9550e347aa7cd145a51e69.

--- test/advanced/TestBaseUtilAdvanced.t.sol
+++ test/advanced/TestBaseUtilAdvanced.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "node_modules/forge-std/src/Test.sol";
+import "forge-std/Test.sol";
 import { MSAAdvanced } from "src/MSAAdvanced.sol";
 import "src/interfaces/IERC7579Account.sol";
 import "src/MSAFactory.sol";
